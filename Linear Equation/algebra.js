e15ea21ba9295ae225723e2eae941a9c51af2795
Nick G <nickwgonzales@gmail.com>
2017-06-14 21:07:34 -0500

    linear equation boilerplate

---
+++ Linear Equation/algebra.js
@@ -0,0 +1,13 @@
+(function exe(canv){
+  const cx = canv.getContext("2d");
+  const inputs = document.querySelector("inputs");
+  const m = inputs[0];
+  const x = inputs[1];
+  const b = inputs[2];
+
+  /*
+    Todo:
+    1. Generate equation or value for given params
+    2. Generate graph for given prarams
+  */
+})(document.querySelector("canvas"));
