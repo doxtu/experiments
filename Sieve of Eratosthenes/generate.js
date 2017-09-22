edb0e6a80763b18bf25155976da407a70c4a4f54
doxtu <nickwgonzales@gmail.com>
2017-09-22 13:33:43 -0500

    Explicit encoding

--- Sieve of Eratosthenes/generate.js
+++ Sieve of Eratosthenes/generate.js
@@ -2,11 +2,11 @@ const sieve = require("./sieve");
 const fs = require("fs");
 const numprimes = 1E5;
 //clear prime file
-fs.writeFileSync("primes.txt","");
+fs.writeFileSync("primes.txt","","utf8");
 console.log("Calculating primes...");
 var primes = sieve(numprimes);
 console.log("Complete!");
 primes.forEach((num,i)=>{
-  if((i+1)%1000 === 0) fs.appendFileSync("primes.txt","\n");
-  fs.appendFileSync("primes.txt",num+" ");
+  if((i+1)%1000 === 0) fs.appendFileSync("primes.txt","\n","utf8");
+  fs.appendFileSync("primes.txt",num+" ","utf8");
 });
