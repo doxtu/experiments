709a15f7fe0978815224a71e9fa6d40e7c1bc6ed
doxtu <nickwgonzales@gmail.com>
2017-09-23 19:43:44 -0500

    socket edits

--- socketio/index.js
+++ socketio/index.js
@@ -2,22 +2,47 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+const users = {};
+const names = ['nick','kim','puppy','otherpuppy','nonpuppy','doxtu','yovo','sveox','ScumFuckFlowerGirl'];
+
 app.get("/",(req,res)=>{
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection',(socket)=>{
-	console.log('user connected');
-	socket.on('disconnect',()=>{
-		console.log('disconnect lmao');
-	});
-	
-	socket.on('chat message',(msg)=>{
-		console.log('user sent:' + msg);
-		io.emit('chat message',msg);
-	});
+  socket.on('disconnect',()=>{
+	  // console.log('disconnect lmao');
+  });
+  
+  socket.on('id',(id)=>{
+	 let user = users[id];
+	 if(typeof user === "undefined"){
+		user = createUser(socket);  
+		// console.log(user, user.name);
+		io.emit('chat message', user.name + " has connected!");
+	 } 
+  });
+  
+  socket.on('chat message',(msg)=>{
+	  // console.log('user sent:' + msg);
+	  io.emit('chat message',msg);
+  });
 });
 
 http.listen(8000,()=>{
   console.log('server running on port 8000');
 });	
+
+function createUser(sock){
+  let user = {
+    id: getRandom(0,10000),
+	name: names[getRandom(0,names.length)],
+	socket: sock
+  };
+  
+  return user;
+}
+
+function getRandom(start,end){
+  return Math.floor(Math.random()*end + start);
+}
