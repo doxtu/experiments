b64530908778fd036cfb390e8cce36947fd31a10
Nick Gonzales <nickwgonzales@gmail.com>
2021-04-25 21:45:07 -0500

    added catgirl

---
+++ catgirl/catgirl.py
@@ -0,0 +1,80 @@
+'''
+    Functions for downloading and emailing catgirls
+
+    Catgirls are binary representation of images of drawings of girls that look like cats. We can save and send these binary representations to people - yay.
+'''
+import os
+
+LOGIN = os.environ.get('DANBOORU_USERNAME')
+API_KEY = os.environ.get('DANBOORU_API_KEY')
+URL = 'https://danbooru.donmai.us'
+ACTION = '/posts.json'
+TAGS = 'cat_girl%20large_breasts'
+PARAMS = f'?login={LOGIN}&api_key={API_KEY}&tags={TAGS}&random=true&limit=1'
+
+
+def get_random_catgirl():
+    '''Get a binary representation of an image from Danbooru's API for a random catgirl'''
+
+    from requests import get
+
+    ret = b''
+
+    try:
+        r = get(f'{URL}{ACTION}{PARAMS}')
+        if r.status_code == 200:
+            data = r.json()
+            # The catgirl is not worthy unless she has a large file size ;)
+            if 'large_file_url' not in data[0]: return get_random_catgirl()
+            image_url = data[0]['large_file_url']
+            ir = get(image_url)
+            ret = ir.content
+        else:
+            os.stderr.write(f'CATERROR - Failed to retrieve catgirl {r.status_code}')
+    except:
+        pass
+
+    return ret
+
+
+def dl_random_catgirl(random_catgirl=None):
+    if random_catgirl == None:
+        random_catgirl = get_random_catgirl()
+
+    import json
+    
+    from random import randrange
+    filename = str(randrange(1,99999999))
+
+    from PIL import Image
+    from io import BytesIO
+    with Image.open(BytesIO(random_catgirl)) as i:
+        file_type = i.format
+        i.save(f'{filename}.{file_type}')
+
+
+FROM_ADDRESS = os.environ.get('NICK_EMAIL')
+TO_ADDRESS = os.environ.get('KIM_EMAIL')
+
+def email_random_catgirl(random_catgirl=None, recipients=[FROM_ADDRESS, TO_ADDRESS]):
+    import smtplib, os
+    from email.message import EmailMessage
+
+    if random_catgirl == None:
+        random_catgirl = get_random_catgirl()
+
+    dl_random_catgirl(random_catgirl)
+
+    PASSWORD = os.environ.get('G_PASSWORD')
+
+    message = EmailMessage()
+    message['Subject'] = 'Catgirl of the day!'
+    message['From'] = FROM_ADDRESS
+    message['To'] = recipients
+    message.set_content('''Cute catgirl is here to brighten up your day!''')
+
+    message.add_attachment(random_catgirl, maintype='image', subtype='jpg', filename='random-catgirl')
+    
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(FROM_ADDRESS,PASSWORD)
+        smtp.send_message(message)
