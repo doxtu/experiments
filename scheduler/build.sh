43531963327cb0f5f827177da49e8be15bb524a2
Nick Gonzales <nickwgonzales@gmail.com>
2021-05-12 10:56:25 -0500

    Docker build for scheduler

---
+++ scheduler/build.sh
@@ -0,0 +1,7 @@
+mkdir build
+cp scheduler.py ./build/scheduler.py
+cp Dockerfile ./build/Dockerfile
+cp requirements.txt ./build/requirements.txt
+cp ../catgirl/catgirl.py ./build/catgirl.py
+cd build
+docker build -t catgirl-scheduler .
